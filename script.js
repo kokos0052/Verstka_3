ee99d0d53f85d4178872572136df8a015f703b1b
kokos0052 <52009415+kokos0052@users.noreply.github.com>
2021-04-26 18:55:47 +0300

    Add files via upload
    Create a Project

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    $('.about__slider').slick({
+        arrows: false,
+        dots: true
+    });
+});
+
+$(document).ready(function (){
+    $('.revios__slider').slick({
+        dots: true
+    });
+});
